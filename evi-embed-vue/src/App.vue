92fa1dd215ffb97d7ffe7a0c951d7a128a45f862
Zach Krall <zach@zachkrall.com>
2024-05-14 12:59:56 -0400

    add vue example for embed

---
+++ evi-embed-vue/src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import HumeEmbed from './components/HumeEmbed.vue'
+</script>
+
+<template>
+  <HumeEmbed />
+</template>
---
+++ evi-embed-vue/src/components/HumeEmbed.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { EmbeddedVoice as EA } from '@humeai/voice-embed';
+
+const embed = ref(null);
+
+onMounted(() => {
+  // if the embed instance already exists, return
+  if (embed.value !== null) {
+    return;
+  }
+
+  // this creates a new instance of the EmbeddedVoice class
+  embed.value = EA.create({
+    auth: {
+      type: 'apiKey',
+      value: import.meta.env.VITE_HUME_API_KEY
+    },
+    onMessage: (message) => {
+      console.log('received message:', message);
+    }
+  });
+
+  // this will mount the embed iframe to the document body
+  embed.value.mount();
+})
+</script>
+
+<template></template>
