28eb5b42d2ee235533a3aa0ec4f54f49cd2c312d
Zach Krall <zach@zachkrall.com>
2024-07-19 17:51:52 -0400

    update next.js examples (#75)

---
+++ evi-next-js-pages-router/pages/index.tsx
@@ -0,0 +1,39 @@
+import { fetchAccessToken } from '@humeai/voice';
+import { InferGetServerSidePropsType } from 'next';
+import dynamic from "next/dynamic";
+
+const Chat = dynamic(() => import("@/components/Chat"), {
+  ssr: false,
+});
+
+export const getServerSideProps = async () => {
+  const accessToken = await fetchAccessToken({
+    apiKey: String(process.env.HUME_API_KEY),
+    clientSecret: String(process.env.HUME_SECRET_KEY),
+  });
+
+  if (!accessToken) {
+    return {
+      redirect: {
+        destination: '/',
+        statusCode: 500,
+      },
+    };
+  }
+
+  return {
+    props: {
+      accessToken,
+    },
+  };
+};
+
+type PageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+export default function Page({ accessToken }: PageProps) {
+  return (
+    <div className={"grow flex flex-col"}>
+      <Chat accessToken={accessToken} />
+    </div>
+  );
+}
