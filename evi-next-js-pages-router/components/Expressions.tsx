bfd43320a916cb2cf6e7f8147c7d520f17e33fb5
zach <zachary.greathouse@hume.ai>
2024-08-27 18:45:47 -0400

    fixes expressions typing in evi-next-js-pages-router

--- evi-next-js-pages-router/components/Expressions.tsx
+++ evi-next-js-pages-router/components/Expressions.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Hume } from "hume";
 import { expressionColors, isExpressionColor } from "@/utils/expressionColors";
 import { motion } from "framer-motion";
 import { CSSProperties } from "react";
@@ -7,10 +8,12 @@ import * as R from "remeda";
 export default function Expressions({
   values,
 }: {
-  values: Record<string, number>;
+  values: Hume.empathicVoice.EmotionScores | undefined;
 }) {
+  if (!values) return;
+
   const top3 = R.pipe(
-    values,
+    values || {},
     R.entries(),
     R.sortBy(R.pathOr([1], 0)),
     R.reverse(),
@@ -23,45 +26,52 @@ export default function Expressions({
         "text-xs p-3 w-full border-t border-border flex flex-col md:flex-row gap-3"
       }
     >
-      {top3.map(([key, value]) => (
-        <div key={key} className={"w-full overflow-hidden"}>
-          <div
-            className={"flex items-center justify-between gap-1 font-mono pb-1"}
-          >
-            <div className={"font-medium truncate"}>{key}</div>
-            <div className={"tabular-nums opacity-50"}>{value.toFixed(2)}</div>
-          </div>
-          <div
-            className={"relative h-1"}
-            style={
-              {
-                "--bg": isExpressionColor(key)
-                  ? expressionColors[key]
-                  : "var(--bg)",
-              } as CSSProperties
-            }
-          >
+      {top3.map(([key, value]) => {
+        console.log(key, value);
+        return (
+          <div key={key} className={"w-full overflow-hidden"}>
             <div
               className={
-                "absolute top-0 left-0 size-full rounded-full opacity-10 bg-[var(--bg)]"
+                "flex items-center justify-between gap-1 font-mono pb-1"
               }
-            />
-            <motion.div
-              className={
-                "absolute top-0 left-0 h-full bg-[var(--bg)] rounded-full"
+            >
+              <div className={"font-medium truncate"}>{key}</div>
+              <div className={"tabular-nums opacity-50"}>
+                {value.toFixed(2)}
+              </div>
+            </div>
+            <div
+              className={"relative h-1"}
+              style={
+                {
+                  "--bg": isExpressionColor(key)
+                    ? expressionColors[key]
+                    : "var(--bg)",
+                } as CSSProperties
               }
-              initial={{ width: 0 }}
-              animate={{
-                width: `${R.pipe(
-                  value,
-                  R.clamp({ min: 0, max: 1 }),
-                  (value) => `${value * 100}%`,
-                )}`,
-              }}
-            />
+            >
+              <div
+                className={
+                  "absolute top-0 left-0 size-full rounded-full opacity-10 bg-[var(--bg)]"
+                }
+              />
+              <motion.div
+                className={
+                  "absolute top-0 left-0 h-full bg-[var(--bg)] rounded-full"
+                }
+                initial={{ width: 0 }}
+                animate={{
+                  width: `${R.pipe(
+                    value,
+                    R.clamp({ min: 0, max: 1 }),
+                    (value) => `${value * 100}%`,
+                  )}`,
+                }}
+              />
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
