2e01bfe7280fd2264b598cc8e0c2075d5969516c
twitchard <richard.marmorstein@gmail.com>
2024-11-21 13:04:21 -0800

    didn't mean to add this

--- evi-react-native-example/modules/example/android/src/main/java/expo/modules/example/ExampleView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.example
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExampleView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
