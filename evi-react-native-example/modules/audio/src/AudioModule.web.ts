8cbc6176fd15754dcbaf460479037517a545a17b
twitchard <richard.marmorstein@gmail.com>
2024-11-21 13:01:18 -0800

    wip

--- evi-react-native-example/modules/audio/src/AudioModule.web.ts
+++ evi-react-native-example/modules/audio/src/AudioModule.web.ts
@@ -9,6 +9,44 @@ let audioStream: MediaStream | null = null;
 let currentAudio: HTMLAudioElement | null = null;
 let isMuted = false;
 
+/**
+ * An AudioClip is a function that you can call to play some audio.
+ * It returns a promise that is resolved when the audio is finished playing.
+ */
+type AudioClip = () => Promise<void>;
+
+// EVI can send audio output messages faster than they can be played back.
+// It is important to buffer them in a queue so as not to cut off a clip of
+// playing audio with a more recent clip. `audioQueue` is a global
+// audio queue that manages this buffering.
+const audioQueue = {
+  clips: [] as Array<AudioClip>,
+  currentlyPlaying: false,
+
+  advance() {
+    if (this.clips.length === 0) {
+      this.currentlyPlaying = false;
+      return;
+    }
+    const nextClip = this.clips.shift()!;
+    nextClip().then(() => this.advance());
+    this.currentlyPlaying = true;
+  },
+
+  add(clip: AudioClip) {
+    this.clips.push(clip);
+
+    if (!this.currentlyPlaying) {
+      this.advance();
+    }
+  },
+
+  clear() {
+    this.clips = [];
+    this.currentlyPlaying = false;
+  },
+};
+
 const mimeType: MimeType = (() => {
   const result = getBrowserSupportedMimeType();
   return result.success ? result.mimeType : MimeType.WEBM;
@@ -51,15 +89,14 @@ export default {
     console.log('Audio recording stopped.');
   },
 
-  async playAudio(base64EncodedAudio: string): Promise<void> {
-    console.log('Playing audio...');
-    return new Promise((resolve) => {
+  async enqueueAudio(base64EncodedAudio: string): Promise<void> {
+    audioQueue.add(() => new Promise((resolve) => {
       const audioBlob = convertBase64ToBlob(base64EncodedAudio, mimeType!);
       const audioUrl = URL.createObjectURL(audioBlob);
       currentAudio = new Audio(audioUrl);
       currentAudio.onended = () => resolve()
       currentAudio.play();
-    })
+    }))
   },
 
   async mute(): Promise<void> {
