a34c16bae2a04e6c6a99d441946d8f08b497706d
twitchard <richard.marmorstein@gmail.com>
2025-04-25 14:13:34 -0700

    use legacy cilent

--- expression-measurement/streaming/python-example/main.py
+++ expression-measurement/streaming/python-example/main.py
@@ -7,18 +7,15 @@ This example takes text input from stdin and outputs emotion summaries to stdout
 import asyncio
 import os
 import sys
-from typing import List, Tuple, TypedDict
+from typing import Dict, List, Tuple, TypedDict, cast
 from dotenv import load_dotenv
 
-from hume import AsyncHumeClient
-from hume.expression_measurement.stream.stream.types.config import Config
-from hume.expression_measurement.stream.stream.types.stream_language import StreamLanguage
-from hume.expression_measurement.stream.stream.types.stream_model_predictions import StreamModelPredictions
-from hume.expression_measurement.stream.stream.types.subscribe_event import SubscribeEvent
+from hume.legacy import HumeStreamClient
+from hume.legacy.models.config import LanguageConfig
 
 load_dotenv()
 
-API_KEY = os.getenv("HUME_API_KEY")
+API_KEY = cast(str, os.getenv("HUME_API_KEY"))
 if not API_KEY:
     print("Error: HUME_API_KEY environment variable not set")
     print("Please create a .env file with your API key")
@@ -28,18 +25,18 @@ class Result(TypedDict):
     text: str | None
     scores: List[Tuple[str, float]]
 
-def process_emotion_scores(event: StreamModelPredictions) -> List[Result]:
-    if not hasattr(event, 'language') or event.language is None or event.language.predictions is None or len(event.language.predictions) == 0:
+def process_emotion_scores(event: Dict) -> List[Result]:
+    if event['language'] is None or event['language']['predictions'] is None or len(event['language']['predictions']) == 0:
         raise ValueError("Unexpected: event does not contain language data")
 
     ret: List[Result] = []
-    for prediction in event.language.predictions:
+    for prediction in event['language']['predictions']:
         ret.append({
             "scores": sorted(
-              [(item.name, item.score) for item in prediction.emotions or [] if item.name and item.score is not None],
+              [(item['name'], item['score']) for item in prediction['emotions'] or [] if item['name'] and item['score'] is not None],
               key=lambda x: x[1],
               reverse=True),
-            "text": prediction.text,
+            "text": prediction['text'],
         })
     return ret
 
@@ -56,16 +53,16 @@ def print_emotion_summary(result: Result) -> None:
 
 async def streaming_example() -> None:
     """Main function to run the streaming example"""
-    client = AsyncHumeClient(api_key=API_KEY)
+    client = HumeStreamClient(api_key=API_KEY)
     
-    language_config = StreamLanguage(granularity="sentence")
-    config = Config(language=language_config)
+    language_config = LanguageConfig(granularity="sentence")
+    configs = [language_config]
     
     print("Hume Language Emotion Streaming Example")
     print("-" * 60)
     
-    async with client.expression_measurement.stream.connect(
-        options={"config": config},
+    async with client.connect(
+        configs
     ) as socket:
         print("\nEnter text and press Enter to analyze emotions")
         
@@ -81,9 +78,6 @@ async def streaming_example() -> None:
                 
                 response = await socket.send_text(text=text)
                 
-                if (not response or not isinstance(response, StreamModelPredictions)):
-                    print("Error: Invalid response from server")
-                    continue
                 results = process_emotion_scores(response)
                 for result in results:
                     print_emotion_summary(result)
