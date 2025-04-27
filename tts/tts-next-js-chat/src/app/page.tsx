074af51908de324ac872963838788bd5f2c36882
zach <zachary.greathouse@hume.ai>
2025-04-27 08:01:03 -0700

    Add Hume logo

--- tts/tts-next-js-chat/src/app/page.tsx
+++ tts/tts-next-js-chat/src/app/page.tsx
@@ -1,17 +1,21 @@
 "use client";
 import Chat from "@/components/Chat";
 import ControlsPanel from "@/components/ControlsPanel";
+import HumeLogo from "@/components/logos/Hume";
 import { VoiceSettingsProvider } from "@/context/VoiceSettingsContext";
 
 export default function Home() {
   return (
-    <div className="min-h-screen w-full bg-neutral-200 flex items-center justify-center px-4 py-20">
-      <VoiceSettingsProvider>
-        <div className="flex h-[80vh] w-full max-w-screen-xl bg-white rounded-3xl shadow-xl overflow-hidden">
-          <Chat />
-          <ControlsPanel />
-        </div>
-      </VoiceSettingsProvider>
-    </div>
+    <>
+      <HumeLogo className={"absolute left-6 top-6 h-5 w-auto fill-black"} />
+      <div className="min-h-screen w-full bg-neutral-200 flex items-center justify-center px-4 py-20">
+        <VoiceSettingsProvider>
+          <div className="flex h-[80vh] w-full max-w-screen-xl bg-white rounded-3xl shadow-xl overflow-hidden">
+            <Chat />
+            <ControlsPanel />
+          </div>
+        </VoiceSettingsProvider>
+      </div>
+    </>
   );
 }
