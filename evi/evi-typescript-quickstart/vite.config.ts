eaa99137b5b593e80c8e7c44fa5844d5dcf1adcb
zach <zachary.greathouse@hume.ai>
2025-05-15 20:34:05 -0700

    fix import aliases

--- evi/evi-typescript-quickstart/vite.config.ts
+++ evi/evi-typescript-quickstart/vite.config.ts
@@ -4,8 +4,8 @@ import path from "path";
 export default defineConfig({
   resolve: {
     alias: {
-      "@styles": path.resolve(__dirname, "src/styles/*"),
-      "@lib": path.resolve(__dirname, "src/lib/*"),
+      "@styles": path.resolve(__dirname, "src/styles/index.css"),
+      "@lib": path.resolve(__dirname, "src/lib"),
     },
   },
 });
