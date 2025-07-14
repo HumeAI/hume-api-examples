3cea6e5398f465381b0de70ae037182c578d024f
Zachary Greathouse <26175515+zgreathouse@users.noreply.github.com>
2025-07-14 12:53:15 -0700

    cleans up unnecessary utility (#185)

--- evi/evi-next-js-app-router-quickstart/utils/getHumeAccessToken.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-import { fetchAccessToken } from "hume";
-
-export const getHumeAccessToken = async (): Promise<string> => {
-  return await fetchAccessToken({
-    apiKey: String(process.env.HUME_API_KEY),
-    secretKey: String(process.env.HUME_SECRET_KEY),
-  });
-};
