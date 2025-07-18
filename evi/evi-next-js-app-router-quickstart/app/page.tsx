b6df37f415bcfd47c2693462becbc954827152c4
twitchard <richard.marmorstein@gmail.com>
2025-07-18 10:50:31 -0700

    bump evi-next-js-app-router-quickstart

--- evi/evi-next-js-app-router-quickstart/app/page.tsx
+++ evi/evi-next-js-app-router-quickstart/app/page.tsx
@@ -6,6 +6,12 @@ const Chat = dynamic(() => import("@/components/Chat"), {
 });
 
 export default async function Page() {
+  if (!process.env.HUME_API_KEY) {
+    throw new Error("The HUME_API_KEY environment variable is not set.");
+  }
+  if (!process.env.HUME_SECRET_KEY) {
+    throw new Error("The HUME_SECRET_KEY environment variable is not set.");
+  }
   const accessToken = await fetchAccessToken({
     apiKey: String(process.env.HUME_API_KEY),
     secretKey: String(process.env.HUME_SECRET_KEY),
