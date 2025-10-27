5783cbcf807292b87380280082e3172a6e18989c
ivaaan <diykarelia@gmail.com>
2025-10-27 20:05:14 +0100

    rm evi-twilio folder

--- evi/evi-twilio/server/audio_processors/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Audio processors for converting between Twilio and EVI audio formats."""
-
-from .twilio_audio_processor import TwilioAudioProcessor
-from .evi_audio_processor import EviAudioProcessor, AudioProcessingConfig
-
-__all__ = ["TwilioAudioProcessor",
-           "EviAudioProcessor", "AudioProcessingConfig"]
