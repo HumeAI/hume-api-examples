f81ea0a53df38114ccf836cca57833a62e362b4e
ivaaan <diykarelia@gmail.com>
2026-02-04 18:08:57 +0100

    wip

--- evi/evi-next-js-function-calling/app/page.tsx
+++ evi/evi-next-js-function-calling/app/page.tsx
@@ -1,6 +1,9 @@
 import { fetchAccessToken } from "hume";
 import ChatLoader from "@/components/ChatLoader";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function Page() {
   const accessToken = await fetchAccessToken({
     apiKey: String(process.env.HUME_API_KEY),
