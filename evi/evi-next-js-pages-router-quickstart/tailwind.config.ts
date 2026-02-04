c4a5c03c3477fcd34a605366cdcd0b27dd579482
ivaaan <diykarelia@gmail.com>
2026-02-04 17:46:42 +0100

    fix build

--- evi/evi-next-js-pages-router-quickstart/tailwind.config.ts
+++ evi/evi-next-js-pages-router-quickstart/tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
